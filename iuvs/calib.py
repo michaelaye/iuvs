a4a9e942dccecebcf102646c0269ac497507c041
Michael Aye <michael.aye@lasp.colorado.edu>
2015-02-17 21:00:54 +0000

    implementing hm and temp calibration

--- iuvs/calib.py
+++ iuvs/calib.py
@@ -1,4 +1,4 @@
-from numpy import poly1d
+import numpy as np
 from collections import namedtuple
 
 # ;-----Original Message-----
@@ -29,9 +29,7 @@ from collections import namedtuple
 # ;office: 303-492-5682
 
 
-Coefficients = namedtuple('Coefficients', 'C0 C1 C2 C3 C4'.split())
-
-def iuvs_dn_to_temp(dn, inverse=False, hw=False):
+def iuvs_dn_to_temp(dn, inverse=False, det_temp=True):
     """Convert DN to temperature or do the inverse.
 
     Parameters
@@ -41,18 +39,22 @@ def iuvs_dn_to_temp(dn, inverse=False, hw=False):
     inverse : bool, optional
         If set to True, the inverse calibration is performed.
         Default: False
-    hw : bool, optional
-        if set to True, use the thermistor polynomial, appropriate for
-        all analog H/K TLM and case_temp science TLM. Default is use the
-        detector on-chip conversion, appropriate to det_temp science TLM
-        only.
+    det_temp : bool, optional
+        if set to False, use the thermistor polynomial, appropriate for
+        all analog H/K TLM and case_temp science TLM. Default is True, to
+        use the detector on-chip conversion, appropriate to det_temp
+        science TLM only.
+
     Returns
     -------
     float
         temperature in degC or DN, as appropriate to value of `inverse`
     """
 
-    if hw:
+    # convert input to float
+    dn = np.array(dn, dtype='float')
+
+    if not det_temp:
         # ;All of the analog telemetry temperatures use the same split polynomial conversion:
         # ;DN from 0 to 8191: C0=1.8195   C1=-6.221E-3   C2=1.6259E-7   C3=-7.3832E-11   C4=-2.9933E-16
         # ;DN from 8192 to 16382: C0=450.47   C1=-6.5734E-2   C2=3.3093E-6   C3=-5.4216E-11   C4=-2.933E-16
@@ -67,11 +69,29 @@ def iuvs_dn_to_temp(dn, inverse=False, hw=False):
                                     -6.221E-3,
                                     1.6259E-7,
                                     -7.3832E-11,
-                                    -2.9933E-16)
-        poly0 = poly1d(*coeffs0_increasing_order.reverse())
+                                    -2.9933E-16]
+        poly0 = np.poly1d(list(reversed(coeffs0_increasing_order)))
         coeffs1_increasing_order = [450.47,
                                     -6.5734e-2,
                                     3.3093e-6,
                                     -5.4216e-11,
                                     -2.933e-16]
-        poly1 = poly1d(*coeffs1_increasing_order.reverse())
+        poly1 = np.poly1d(list(reversed(coeffs1_increasing_order)))
+
+        if not inverse:
+            w = dn < 8192
+            # create result array
+            result = np.zeros_like(dn)
+            # apply appropriate polynom
+            result[w] = poly0(dn[w])
+            result[~w] = poly1(dn[~w])
+            return result
+    else: # if det_temp == True
+        # for the chip temperatures: (DN * -0.0749) + 174.08
+        a = -0.0749
+        b = 174.08
+        if not inverse:
+            return a*dn + b
+        else:
+            T = dn
+            return (T-b) / a
