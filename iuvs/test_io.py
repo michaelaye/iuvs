dd401fd5d834c33ad95935dec38d7fd92bd39554
Michael Aye <michael.aye@lasp.colorado.edu>
2015-05-20 00:42:44 +0000

    adding test_iuvs_utc_to_dtime test

--- iuvs/test_io.py
+++ iuvs/test_io.py
@@ -1,6 +1,16 @@
-import numpy as np
+def test_iuvs_utc_to_dtime():
+    from .io import iuvs_utc_to_dtime
+    import datetime as dt
+    s = '2015/085 Mar 26 17:45:19.96275UTC'
+    expected = dt.datetime.strptime("2015-03-26 17:45:19.962750",
+                                    '%Y-%m-%d %H:%M:%S.%f')
+    assert expected == iuvs_utc_to_dtime(s)
 
 
-# def test_fits_write():
-#     arr = np.arange(10000).shape(100,100)
+class TestFitsFile:
 
+    "Collector for test for the FitsFile class."
+
+    def test_init():
+        "init claims it needs an absolute path as input. test this here"
+        pass
