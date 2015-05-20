1d8a6af6e2ad27dbe8ed939b231d6a29c5c7b112
Michael Aye <michael.aye@lasp.colorado.edu>
2015-05-20 19:23:01 +0000

    adding show_versions script

--- setup.py
+++ setup.py
@@ -1,45 +1,27 @@
-# import ez_setup
-# ez_setup.use_setuptools()
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
-pandas_version = '0.15.2'
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ['-v', '-m']
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
+pandas_version = '0.16.0'
 
 setup(
-    name = "IUVS",
-    version = "0.3",
-    packages = find_packages(),
+    name="IUVS",
+    version="0.3",
+    packages=find_packages(),
 
-    install_requires = ['astropy'],
-    # tests_require = ['pytest'],
+    install_requires=['astropy', 'pandas>='+pandas_version],
 
-    cmdclass = {'test': PyTest},
+    scripts=['bin/show_versions'],
 
     # entry_points={
     #     "console_scripts": [
-    #         'scp_l1a = diviner.file_utils:scp_l1a_file'
+    #         'show_versions = iuvs.show_versions'
     #         ]
     # },
 
-    #metadata
-    author = "K.-Michael Aye",
-    author_email = "michael.aye@lasp.colorado.edu",
-    description = "Software for handling MAVEN IUVS data",
-    license = "BSD 2-clause",
-    keywords = "MAVEN IUVS",
-    url = "http://lasp.colorado.edu",
+    # metadata
+    author="K.-Michael Aye",
+    author_email="michael.aye@lasp.colorado.edu",
+    description="Software for handling MAVEN IUVS data",
+    license="BSD 2-clause",
+    keywords="MAVEN IUVS",
+    url="http://lasp.colorado.edu",
 )
