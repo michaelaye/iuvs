29b70519a616861e106124007f4fbbf39a656ede
Michael Aye <michael.aye@lasp.colorado.edu>
2015-10-26 09:42:08 +0000

    streamlining meta to better path handling

--- iuvs/meta.py
+++ iuvs/meta.py
@@ -1,29 +1,21 @@
 import pandas as pd
 from . import io
 
-stagel1asummary = str(io.stagelevel1apath / 'IUVS_L1A_index_summary.txt')
-stagel1bsummary = str(io.stagelevel1bpath / 'IUVS_L1B_index_summary.txt')
-prodl1asummary = str(io.productionlevel1apath / 'IUVS_L1A_index_summary.txt')
-prodl1bsummary = str(io.productionlevel1bpath / 'IUVS_L1B_index_summary.txt')
 
-
-def get_summary_df(fname):
+def get_summary_df(level, env):
+    folder = io.get_data_path(level, env)
+    name = 'IUVS_{}_index_summary.txt'.format(level.upper())
+    fname = folder / name
     return pd.read_table(fname, skiprows=26, skipinitialspace=True,
                          sep=' ', index_col=0)
 
 
-def l1a_summary(stage=True):
-    if stage:
-        return get_summary_df(stagel1asummary)
-    else:
-        return get_summary_df(prodl1asummary)
+def l1a_summary(env='production'):
+    return get_summary_df('l1a', env)
 
 
-def l1b_summary(stage=True):
-    if stage:
-        return get_summary_df(stagel1bsummary)
-    else:
-        return get_summary_df(prodl1bsummary)
+def l1b_summary(env='production'):
+    return get_summary_df('l1b')
 
 
 def get_full_frames(df):
