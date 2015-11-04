1daee8bf4c28040b2e35109ff5a517e9723d8147
Michael Aye <michael.aye@lasp.colorado.edu>
2015-11-04 04:31:25 +0000

    added t1 and t2 plotting between 2 times and orbit file updating.

--- iuvs/hk.py
+++ iuvs/hk.py
@@ -1,18 +1,50 @@
 import argparse
+import datetime as dt
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import spiceypy as spice
 from astropy.io import fits
+from matplotlib.ticker import ScalarFormatter
 from pathlib import Path
 
 from . import io
 from .spice import load_kernels
 
-dbpath = Path('/home/klay6683/to_keep/HK_DB_stage.h5')
+dataroot = Path.home() / 'to_keep'
+dbpath = dataroot / 'HK_DB_stage.h5'
+orbitfile = dataroot / 'orbit_numbers.hdf'
 
 load_kernels()
+
+sns.set_style('darkgrid', {'axes.facecolor': '.9'})
+sns.set_context('talk')
+sns.set_palette(sns.color_palette('husl', 7))
+
+instr_temps = ['FUV_CHIP_TEMP_C',
+               'FUV_DET_TEMP_C',
+               'FUV_INT_TEMP_C',
+               'MUV_CHIP_TEMP_C',
+               'MUV_DET_TEMP_C',
+               'MUV_INT_TEMP_C']
+
+iuvs_temps = ['IUVS_1_TEMP_C',
+              'IUVS_2_TEMP_C',
+              'IUVS_3_TEMP_C',
+              'IUVS_4_TEMP_C']
+
+htr_mot_temps = ['ZONE_1_HTR_TEMP_C', 'ZONE_2_HTR_TEMP_C',
+                 'HV_POWR_TEMP_C', 'GRAT_MOT_TEMP_C', 'SCAN_MOT_TEMP_C']
+
+op_htr_temps = ['OP_1_HTR_TEMP_C',
+                'OP_2_HTR_TEMP_C',
+                'POWER_BD1_TEMP_C',
+                'POWER_BD2_TEMP_C']
+
+temp_groups = [instr_temps, iuvs_temps, htr_mot_temps, op_htr_temps]
 
 
 def format_times(sec, subsec):
@@ -104,16 +136,111 @@ def update_database():
             print("Adding {}".format(index.date()))
             process_hk_fname(str(row.p))
     else:
-        print("Nothing to update.")
+        print("HK database is up to date.")
+
+
+def update_orbit_file(force=False):
+    orbits = pd.read_hdf(str(orbitfile), 'df')
+    today = dt.date.today()
+    sevendays = dt.timedelta(days=7)
+    # check if orbit file already has today's date
+    if (today - orbits.index[-1].date()) < sevendays and not force:
+        print("Orbit file is younger than a week. Won't update.\n"
+              "(Force with '--force', as it takes 1-2 mins.)")
+        return
+
+    print("Updating orbit file.")
+    print("Reading all L1A filenames... [1-2 mins]")
+    l1afnames = io.get_filename_df('l1a', env='stage')
+
+    def get_orbit_number(x):
+        if x.startswith('orbit'):
+            return float(x[5:])
+        else:
+            return np.nan
+
+    l1afnames['orbit'] = l1afnames['cycle_orbit'].map(get_orbit_number)
+    orbits = l1afnames.orbit.dropna().astype(int)
+    orbits.to_hdf(str(orbitfile), 'df')
+    print("Updated orbit file {}.".format(orbitfile))
+
+
+def generate_plot(data, timeres, loffset, title):
+    resampled = data.resample(timeres, base=0).dropna(how='all')
+
+    orbits = pd.read_hdf(str(orbitfile), 'df')
+    orbits = orbits.resample(timeres, loffset=loffset).dropna(how='all')
+
+    resampled['orbit'] = orbits
+
+    for savename, keys in zip(['detector_temps', 'iuvs_temps', 'heater_motor_temps',
+                               'op_htr_temps'],
+                              temp_groups):
+        fig, ax = plt.subplots()
+        try:
+            resampled['orbit'].plot(legend=True, secondary_y=True, ax=ax)
+            resampled[keys].plot(ax=ax, style='*', legend=True, markersize=12)
+        except TypeError:
+            continue
+        ax.set_ylabel('Temperature [$C^\circ$]')
+        ax.right_ax.set_ylabel('Orbit number')
+        ax.set_title('{}, {} means'.format(title, timeres))
+        ax.right_ax.yaxis.set_major_formatter(ScalarFormatter(useOffset=False))
+        ax.set_ylim(resampled[keys].min().min()-1.5,
+                    resampled[keys].max().max()+1.5)
+        savepath = '{}_{}.png'.format(savename, title.replace(' ', '_'))
+        fig.savefig(savepath, dpi=200)
+        print('Created', savepath)
+        plt.close(fig)
+
+
+def plot_date(timestring, timeres='600s'):
+    print("Reading data...")
+    with pd.HDFStore(str(dbpath)) as store:
+        c = store.select_column('df', 'index')
+        where = c[pd.DatetimeIndex(c).date == pd.Timestamp(timestring).date()].index
+        data = store.select('df', where=where)
+    print("Generating plots...")
+    generate_plot(data, timeres, 0, timestring)
+
+
+def plot_times(t1, t2, timeres):
+    print("Reading data...")
+    data = pd.read_hdf(str(dbpath), 'df',
+                       where="index>=t1 and index<t2")
+    print("Generating plots...")
+    generate_plot(data, timeres, 0, '{}-{}'.format(t1, t2))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('cmd', choices=['status', 'update'],
+    parser.add_argument('cmd', choices=['status', 'update', 'plot'],
                         help="Choose one of the available sub-commands.")
-
+    parser.add_argument('--date', type=str,
+                        help="Provide the date string in YYYYMMDD format.")
+    parser.add_argument('--force', action='store_true', default=False,
+                        help='Enforce the activity if rejected by program.')
+    parser.add_argument('--t1', type=str,
+                        help="Start time for plot [UTC format].")
+    parser.add_argument('--t2', type=str,
+                        help="End time for plot [UTC format].")
+    parser.add_argument('--timeres', type=str,
+                        help='Time resolution for resampling in seconds.')
     args = parser.parse_args()
     if args.cmd == 'status':
         check_database_status()
     elif args.cmd == 'update':
         update_database()
+        update_orbit_file(args.force)
+    elif args.cmd == 'plot':
+        if args.date:
+            if not args.timeres:
+                args.timeres = '600s'
+            plot_date(args.date, args.timeres)
+        elif len(args.t1) > 0 and len(args.t2) > 0:
+            if not args.timeres:
+                args.timeres = '1800s'
+            plot_times(args.t1, args.t2, args.timeres)
+
+if __name__ == '__main__':
+    sys.exit(main())
